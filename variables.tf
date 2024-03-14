f787a8f3cec35400009fea10a6d169471b2d8cb9
Dzmitry Bichko <bichko@AZ.LOCAL>
2024-03-14 11:12:12 +0300

    add ALB Temlpate TargetGroup AutoScalingGroup

--- variables.tf
+++ variables.tf
@@ -81,3 +81,66 @@ variable "public-rt-name" {
 variable "private-rt-name" {
   description = "Name for Private Route table"
 }
+variable "launch-template-web-name" {
+  description = "Name for Launch-template-1"
+}
+
+variable "image-id" {
+  description = "Value for Image-id"
+}
+
+variable "instance-type" {
+  description = "Value for Instance type"
+}
+
+variable "key-name" {
+  description = "Value for Key name"
+}
+
+variable "web-instance-name" {
+  description = "Value for Web Instances"
+}
+
+variable "alb-web-name" {
+  description = "Name the Load Balancer for the Web Tier"
+}
+
+variable "alb-sg-web-name" {
+  description = "Name for alb security group 1"
+}
+
+variable "asg-web-name" {
+  description = "Name the Auto Scaling group in Web Tier"
+}
+
+variable "asg-sg-web-name" {
+  description = "Name for asg security group 1"
+}
+
+variable "tg-web-name" {
+  description = "Name for Target group web"
+}
+
+variable "launch-template-app-name" {
+  description = "Name for Launch-template-1"
+}
+
+variable "app-instance-name" {
+  description = "Value for App Instances"
+}
+
+variable "alb-app-name" {
+  description = "Name the Load Balancer for the App Tier"
+}
+
+variable "alb-sg-app-name" {
+  description = "Name for alb security group 1"
+}
+
+variable "asg-app-name" {
+  description = "Name the Auto Scaling group in app Tier"
+}
+
+variable "asg-sg-app-name" {
+  description = "Name for asg security group 1"
+}
